38767db92cc66b1b1775cbc28980becce81ba53b
Quan Tran <quan.tran@isobar.com>
2018-08-13 17:21:11 +0700

    connect to mongodb

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ var bodyParser = require('body-parser');
 var multer = require('multer');
 var upload = multer();
 var app = express();
+// connect to mongo db
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/express');
+mongoose.connect('mongodb://localhost:3000/express', {useNewUrlParser: true});
 
-app.get('/', function(req, res){
-   res.render('form');
+
+
+var personSchema = mongoose.Schema({
+    name: String,
+    age: Number,
+    nationality: String
 });
+var Person = mongoose.model("Person", personSchema);
+// router: add new document to db
+app.get('/person', function(req, res){
+    res.render('person');
+});
+
 
 app.set('view engine', 'pug');
 app.set('views', './views');
@@ -25,7 +36,34 @@ app.use(upload.array());
 app.use(express.static('public'));
 
 app.post('/', function(req, res){
-   console.log(req.body);
-   res.send("recieved your request!");
+   res.send("received your request!");
+});
+
+// router: post, create new document
+app.post('/person', function(req, res){
+    var personInfo = req.body; //Get the parsed information
+
+    if(!personInfo.name || !personInfo.age || !personInfo.nationality){
+        res.render('show_message', {
+            message: "Sorry, you provided worng info", type: "error"});
+    } else {
+        var newPerson = new Person({
+            name: personInfo.name,
+            age: personInfo.age,
+            nationality: personInfo.nationality
+        });
+            
+        newPerson.save(function(err, Person){
+            if(err)
+                res.render('show_message', {message: "Database error", type: "error"});
+            else
+                res.render('show_message', {
+                message: "New person added", type: "success", person: personInfo});
+        });
+    }
+});
+
+app.get('/', function(req, res){
+   res.render('form');
 });
 app.listen(3000);
